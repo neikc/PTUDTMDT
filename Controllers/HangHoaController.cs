080096a0a8e12f71960f72e985feeb1fcb206171
Châu Chí Kiện <kienchau.31221022673@st.ueh.edu.vn>
2024-10-18 17:18:29 +0700

    - Việt hóa trang 404 (xóa Header và Footer html trong file 404) - Chỉnh sửa layout _Header và _Footer (việt hóa)

---
+++ Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTUDTMDT.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
